b8d6a760cadf3c015e1d29ecf6ed49dfb4af5425
1st-one <fantazuy228@gmail.com>
2021-04-12 11:12:15 +0300

    bind server requests

--- src/page/table/Table.jsx
+++ src/page/table/Table.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import icon from '../../imgs/defaultIcon.png';
 
-const Table = ({ data }) => {
+const Table = ({ data, value, isLoading }) => {
+    useEffect(() => {
+        console.log(data)
+    })
 
     return (
         <div className="table">
             <span className="table__label">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                {value ? `Вот информация по вашему запросу '${value}'` : null}
             </span>
             <div className="table__list">
-                {data.map(item => {
+                {isLoading 
+                ? data.map(item => {
                     return (<div key={item.imdbID} className="table__list-item">
                         <span className="name">{item.Title}</span>
-                        <img src={item.Poster} alt="" />
+                        <img src={item.Poster !== 'N/A' ? item.Poster : icon} alt="" />
                         <span className="year">{item.Year}</span>
                     </div>)
-                })}
+                })
+                : <h1 className='eps'>Films not found</h1>
+            }
             </div>
         </div>
     );
