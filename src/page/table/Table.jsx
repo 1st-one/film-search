3c5146b9add0fbb2240c00513bc41c038a806567
1st-one <fantazuy228@gmail.com>
2021-04-14 20:03:28 +0300

    filmpage/redux ready

--- src/page/table/Table.jsx
+++ src/page/table/Table.jsx
@@ -1,33 +1,13 @@
 import React from 'react';
-import icon from '../../imgs/defaultIcon.png';
-import { Link } from 'react-router-dom';
+import TableList from './TableList';
 
-const Table = ({ data, value, isLoading, posterHandler }) => {
+const Table = ({ value, posterHandler }) => {
     return (
         <div className="table">
             <span className="table__label">
                 {value ? `Вот информация по вашему запросу '${value}'` : null}
             </span>
-            <iframe 
-                title="prev" width="500" height="300" src="https://www.youtube.com/embed/8ugaeA-nMTc" 
-                frameBorder="1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-                allowFullScreen>
-            </iframe>
-            <div className="table__list">
-                {isLoading 
-                    ? data.map(item => {
-                        return (
-                            <Link to={`/film/${item.imdbID}/${item.Title}`}>
-                                <div key={item.imdbID} className="table__list-item">
-                                    <span className="name">{item.Title}</span>
-                                    <img src={item.Poster !== 'N/A' ? item.Poster : icon} onClick={() => posterHandler(item.imdbID)} alt="" />
-                                    <span className="year">{item.Year}</span>
-                                </div>
-                            </Link>
-                            )})
-                    : <h1 className='eps'>Films not found</h1>
-                } 
-            </div>         
+            <TableList posterHandler={posterHandler}/>
         </div>
     );
 };
